632327033fc9a8b92a007ed057fc8a4931994303
Taha <staha218@hotmail.com>
2024-12-26 23:17:10 +0500

    commint ok

---
+++ all-about-research-integrity-a-beginner-guide-in-depth-course.php
@@ -0,0 +1,126 @@
+<?php include('header.php'); ?>
+
+<section class="white-section allaboutresearch">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-md-2 col-12">
+                <div class="share-icon">
+                    <p class="text-center">Share</p>
+                    <ul>
+                        <li> <a href="#"><i class="fa-brands fa-facebook-f"></i></a> </li>
+                        <li> <a href="#"><i class="fa-brands fa-twitter"></i></a> </li>
+                        <li> <a href="#"><i class="fa-brands fa-linkedin-in"></i></a> </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-10 col-md col-12">
+                <h3 class="subheading">All about research integrity: a beginner's guide & an in-depth course</h3>
+                
+                <div class="row mt-4 mb-4">
+                    <div class="rdiv">
+                        <div class="rlogo">
+                            <h5>t</h5>
+                        </div>
+                        <div class="rtext">
+                            <p class="mb-0">The Source</p>
+                            <p class="mb-0">By: Guest contributor, Fri Aug 4 2023</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row mb-5">
+                    <div class="col-lg-8 my-auto">
+                        <p class="textinfo">The Springer Nature transfer service has expanded rapidly in recent years, and it is now a deeply-integrated aspect of submissions for most of our portfolios.</p>
+                        <p class="textinfo">To help you come to grips with what this means, Springer Nature has developed a free, self-led course that will give you a solid foundation in research integrity. Among the topics covered: Study design, publication ethics, data integrity, clinical trial ethics, and more. </p>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="row align-items-center border-1 py-3">
+                            <div class="text-box">
+                                <div>
+                                    <i class="fa-solid fa-user"></i> 
+                                </div>
+                                <div>
+                                    <p>Author: Guest contributor</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row mt-4">
+                    <h4 class="textgray mb-4">Study design and execution</h4>
+                    <p>Carrying out the research with integrity includes clear and scrupulously honest documentation of what you do and the results obtained, complying with all appropriate guidelines and requirements for the type of research being carried out, and accurate, unbiased analysis of the results obtained. Poor planning or poor design can lead to a plethora of consequences (especially true if your work involves hazardous materials or procedures in any way). </p>
+                    <p>Your results can be rendered meaningless, resources and budget can be wasted, your reputation and credibility damaged and any publications from the work impacted. Funding (present or future) can then be affected. By contrast, well researched and conducted work will be more likely to answer the question you are investigating and better able to stand up to scrutiny such as peer review. This can also have a positive effect on reproducibility and future reputation.</p>
+                </div>
+                
+                <div class="row mt-4">
+                    <h4 class="textgray mb-4">Data integrity and management</h4>
+                    <p>Once research has begun, it is important to ensure that the resulting data, in whatever format it is produced, adheres to some basic tenets:</p>
+                    
+                    <div class="list-1 list-3 mt-4">
+                        <ul>
+                            <li><i class="fa-solid fa-check"></i> Data should never be fabricated, i.e. made up wholly or in part. This might seem obvious, but is still sadly prevalent, as seen in these high-profile examples from anesthesiology, evolutionary biology and biotechnology.</li> 
+                            <li><i class="fa-solid fa-check"></i> Data should never be falsified. Analysis of data is obviously a key part of the research process, but this process should not change or omit data such that the research is no longer accurately represented. This would include removing individual data points or participants without appropriate justification, changing the brightness/contrast of a part of an image to enhance certain features etc.</li> 
+                            <li><i class="fa-solid fa-check"></i> Data should be stored and archived, as well as backed up, to ensure integrity is maintained.</li>
+                            <li><i class="fa-solid fa-check"></i> Maintain accurate metadata. Metadata refers to information about your data such as the project name, dates of collection, information about how and by whom data was collected and key parameters, units of measurement, etc. This is a crucial area with a lot of detail, and there is a detailed course, Managing Research Data, available from Nature Masterclasses to help.</li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="row mt-4">
+                    
+                    <h4 class="textgray mb-4">Avoiding citation manipulation</h4>
+                    
+                    <p>Citation manipulation misrepresents the importance of a particular piece of work and the journal in which it appears. It results in distortion of the scholarly record. If a poor-quality study is cited, this in turn can result in anything from wasted time (as other researchers try to replicate or build on results that are of less importance or soundness than implied) to implications for such things as healthcare decisions. Funding or employment opportunities may be misapplied as a consequence.</p>
+                </div>
+                
+                <div class="row mt-4">
+                    <div class="list-1 list-3">
+                        <ul>
+                              <li> <div><i class="fa-solid fa-check"></i></div> <div> Do ensure you clearly and accurately reference publications that are relevant to your work and manuscript.</div> <li>
+                              <li> <div><i class="fa-solid fa-check"></i></div> <div> Don't include references only as favors to colleagues or in return for anything they offer you. </div><li>
+                              <li> <div><i class="fa-solid fa-check"></i></div> <div> Do consider whether reviewer requests to include citations would enhance the paper or not (if the journal uses open peer review, where reviewers are identified to the authors, you may be able to tell when a reviewer is requesting references to their own work). If the reference would not enhance the paper or is not relevant to your work, you should point this out in your response comments and/or flag it to the Editor.</div> <li>
+                              <li> <div><i class="fa-solid fa-check"></i></div> <div> Do consider requests from journals in a similar way, and politely decline them if the requested citations are not relevant. If a journal appears to be committing citation manipulation deliberately, e.g. rejecting manuscripts for refusing to include irrelevant citations, consider reporting the matter to the Publisher for investigation. Journals can face serious consequences for citation manipulation.</div><li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <h4>To learn more about how to prepare for your next article ethically and with best practice for research integrity, you can take Springer Nature’s free online tutorial, “Research integrity — an introduction for researchers.” </h4>
+                    <p>You can also explore more resources at Nature Masterclasses and at AJE.</p>
+                </div>
+                
+                <div class="row mt-4">
+                    <div class="col-lg-8">
+                        <div class="text-box border-1 p-3">
+                            <div>
+                                <i class="fa-solid fa-user"></i> 
+                            </div>
+                            <div>
+                                <p>Author: Guest contributor</a></p>
+                                <p>Guest Contributors include Springer Nature staff and authors, industry experts, society partners, and many others. If you are interested in being a Guest Contributor, please contact us via email: thesource@springernature.com.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row mt-5">
+                    <div class="col-12 p-0">
+                        <p><strong>Related Tags:</strong></p>
+                        <div class="buttonlinks mt-4">
+                            <div class="button011">
+                                <a href="#">Research Integrity</a>
+                            </div>
+                            <div class="button011">
+                                <a href="#">author services</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
---
+++ five-key-takeaways-from-our-science-for-a-sustainable-future.php
@@ -0,0 +1,179 @@
+<?php include('header.php'); ?>
+
+<section class="white-section fivekey-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-md-2 col-12">
+                <div class="share-icon">
+                    <p class="text-center">Share</p>
+                    <ul>
+                        <li> <a href="#"><i class="fa-brands fa-facebook-f"></i></a> </li>
+                        <li> <a href="#"><i class="fa-brands fa-twitter"></i></a> </li>
+                        <li> <a href="#"><i class="fa-brands fa-linkedin-in"></i></a> </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-10 col-md-10 col-12">
+                <div class="row">
+                    <h3 class="subheading border-none">Five key takeaways from our Science for a Sustainable Future webinar series</h3>
+                </div>
+                <div class="row mt-4 mb-4">
+                    <div class="rdiv">
+                        <div class="rlogo">
+                            <h5>t</h5>
+                        </div>
+                        <div class="rtext">
+                            <p class="mb-0">The Source</p>
+                            <p class="mb-0">By: Guest contributor, Thu Jul 13 2023</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="col-lg-8 my-auto">
+                        <p class="textinfo">Science for a Sustainable Future is a joint initiative of the United Nations Sustainable Development Solutions Network (UN SDSN) and Springer Nature. In April 2023 we co-hosted three webinars focusing on energy (SDG 7). In this blog we cover some of the highlights and learnings from the webinars. Below you can also find a summary illustration of each session:</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="row align-items-center border-1 py-3">
+                            <div class="text-box">
+                                <div>
+                                    <i class="fa-solid fa-user"></i> 
+                                </div>
+                                <div>
+                                    <p>Author: Guest contributor</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="list-1 list-3">
+                        <ul>
+                            <li>
+                               <i class="fa-solid fa-check"></i> Energy and the Transition to Net Zero, moderated by Maher Chebbo, Managing Director of Envision Digital Europe.  
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <img src="assets/images/blog/154.webp" class="img-fluid" alt=""/>
+                </div>
+                 <div class="row mt-4">
+                    <div class="list-1 list-3">
+                        <ul>
+                            <li>
+                               <i class="fa-solid fa-check"></i> Energy, Social Justice and the Global South, moderated by Nature Editor-in-Chief Magdalena Skipper. 
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <img src="assets/images/blog/155.webp" class="img-fluid" alt=""/>
+                </div>
+                <div class="row mt-4">
+                    <div class="list-1 list-3">
+                        <ul>
+                            <li>
+                               <i class="fa-solid fa-check"></i> Energy and Science Communication, moderated by Mark Fischetti, Senior Editor at Scientific American.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <img src="assets/images/blog/156.webp" class="img-fluid" alt=""/>
+                </div>
+                 <div class="row mt-4">
+                     <p>Panellists included: </p>
+                    <div class="list-1 list-3 mt-4 mb-4">
+                        <ul>
+                            <li> <i class="fa-solid fa-check"></i> Wei-Jen Lee, Professor and Director, Energy Systems Research Center, University of Texas at Arlington, USA </li>
+                            <li> <i class="fa-solid fa-check"></i> Irene Cañas Díaz, Chief Executive Officer, Costa Rican Institute of Electricity (ICE), Costa Rica </li>
+                            <li> <i class="fa-solid fa-check"></i> Francesca Larosa, Research Fellow at the UCL Institute for Sustainable Resources, UK </li>
+                            <li> <i class="fa-solid fa-check"></i> Rosemary Idem, Program Manager for Women and Youth at the Forefront at Sustainable Energy for All, USA </li>
+                            <li> <i class="fa-solid fa-check"></i> Elisa Puzzolo, Senior Research Fellow in Global Health at the University of Liverpool, UK </li>
+                            <li> <i class="fa-solid fa-check"></i> Abhishek Kar, Senior Program Lead at the Council on Energy, Environment and Water, India </li>
+                            <li> <i class="fa-solid fa-check"></i> Simon Evans, Deputy Editor and Senior Policy Editor, Carbon Brief. </li>
+                            <li> <i class="fa-solid fa-check"></i> Runa Das, Associate Professor in Social Sciences at Royal Roads University, Vancouver, Canada. </li>
+                            <li> <i class="fa-solid fa-check"></i> Hannah Stafford, Senior Policy Advisor at the White House Climate Policy Office, USA. </li>
+                            <li> <i class="fa-solid fa-check"></i> Bruno Takahashi, Professor of Environmental Communication, Michigan State University, USA. </li>
+                        </ul>
+                    </div>
+                    <p>Here we highlight the main discussion themes and offer some key takeaways from the series.</p>
+                </div>
+                <div class="row mt-4">
+                    <h4 class="textgray">1. Co-creating clean energy solutions </h4>
+                    <p>Rosemary Idem emphasised that transition to clean energy must consider regional ways of life to avoid leaving anyone behind. For example, in South America and Latin America >50% of the population can access clean cooking fuel sources, compared with 10% in sub-Saharan Africa. There are also conflicting demands: people in the Global South want to access clean cooking sources, but some regions’ economies rely on fossil fuels. 
+                        Runa Das argued the energy crisis means different things to different people, and that policy setters and decision makers must consider who they listen to. “Gender, race and other identity factors matter for both the questions we ask and the solutions we find,” she said.</p>
+                    <p>“It is very important that we have localised policies,” added Abhishek Kar. “These are diverse regions with national decision-making. Any action towards energy justice needs to be politically and socially acceptable.”</p>
+                </div>
+                <div class="row mt-4">
+                    <h4 class="textgray">2. An enabling environment for clean energy transition</h4>
+                    <p>Around 2.4 billion people globally use fuels like firewood, charcoal, coal and kerosene for their everyday cooking, heating and lighting needs. This has far-reaching impact on health, says Elisa Puzzolo, with the World Health Organization estimating 3.2 million people globally die prematurely each year due to household air pollution. </p>
+                    <p>“A strong, national enabling environment for transition to clean household energy is needed,” says Puzzolo. “This should include fuel price policies, regulatory frameworks for appliances, fuel distribution and use, and enforcement – there is currently no compliance with risk management of these fuels and technologies.”</p>
+                </div>
+                <div class="row mt-4">
+                    <h4 class="textgray">3. Learning from others</h4>
+                    <p>Past attempts to switch consumers to clean energy through reduced energy costs failed, as consumers soon returned to normal habits. Yet, Dr Wei-Jen Lee shared that when Texas energy companies provided night-time solar energy for free and charged a premium rate for day-time solar power, 80-95% residential usage shifted to the free time-zone, demonstrating that consumers will switch to cheaper, cleaner energy sources if the incentives are high enough.</p>
+                    <p>Three quarters of Costa Rica’s carbon emissions comes from transport. Irene Cañas Díaz shared their approach to decarbonising the economy, first educating people on the health impact of air quality and introducing policies to stop import of old, inefficient cars. “Previously, people paid higher tax on newer, efficient cars. We reversed this so new electric cars could be imported tax-free. Now we have cleaner cars circulating and we’re introducing policies to improve public transport.”</p>
+                </div>
+                <div class="row mt-4">
+                    <h4 class="textgray">4. Investing in clean energy-associated technologies </h4>
+                    <p>As clean energy investment grows, investment in unforeseen areas is required, such as lithium supply for battery cells. These industries provide livelihoods and create opportunities for the most vulnerable countries in the world but as Francesca Larosa warned, “We must be mindful that climate action has impacts and repercussions on other sustainable development dimensions. We need to understand the synergies, trade-offs and risks of introducing new regional dividing lines when investing in clean energy transition.”</p>
+                </div>
+                <div class="row mt-4">
+                    <p>Simon Evans contrasted the success of different narratives about climate change, arguing that although Al Gore’s film, An Inconvenient Truth, changed people’s minds, it did not facilitate adoption of climate policy. By contrast, Biden’s Inflation Reduction Act proved more successful as it focused on promising to deliver prosperity to American families.</p>
+                    <p class="mb-4">Researchers trying to influence decision-makers must tailor their pitch to whoever can do most with it, considering policy cycles and decision points, said Hannah Stafford, while Bruno Takahashi urged communicators to think about their objective when deciding on tactics. “Knowledge is a necessary but insufficient factor in decision-making,” he said. “People’s perceptions, values, self-efficacy and social norms are probably more important.” </p>
+
+                    <video
+                        id="my-video"
+                        class="video-js"
+                        controls
+                        preload="auto"
+                        poster="assets/images/review/2.webp"
+                        data-setup="{}">
+                        <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4" type="video/mp4" />
+                    </video>
+                </div>
+                <div class="row mt-4">
+                    <a href="#" class="textblue text-deco">To watch more detailed discussion including Q&A sessions, watch the webinar recordings.</a>
+                </div>
+                <div class="row mt-4">
+                    <p>Discover the key policy recommendations needed to achieve SDG7 that were identified during the series, <a href="#">by downloading our Policy Brief: Delivering Clean, Affordable Energy For All</a>.</p>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-lg-8">
+                        <div class="text-box border-1 p-3">
+                            <div>
+                                <i class="fa-solid fa-user"></i> 
+                            </div>
+                            <div>
+                                <p>Guest Contributors include Springer Nature staff and authors, industry experts, society partners, and many others. If you are interested in being a Guest Contributor, please contact us via email: thesource@springernature.com.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-12 p-0">
+                        <p><strong>Related Tags:</strong></p>
+                        <div class="buttonlinks mt-4">
+                            <div class="button011">
+                                <a href="#">Research Integrity</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                        <div class="share-icon share-icon2">
+                            <p class="text-center">Share</p>
+                            <ul>
+                                <li> <a href="#"><i class="fa-brands fa-facebook-f"></i></a> </li>
+                                <li> <a href="#"><i class="fa-brands fa-twitter"></i></a> </li>
+                                <li> <a href="#"><i class="fa-brands fa-linkedin-in"></i></a> </li>
+                            </ul>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
