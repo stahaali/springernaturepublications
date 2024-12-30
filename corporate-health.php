3222161422dae7e5cc7571844c697958334499fe
asteriksdevteam <devteam@asteriksdigital.com>
2024-12-30 09:29:04 -0800

    commin ok

--- corporate-health.php
+++ corporate-health.php
@@ -1,5 +1,228 @@
 <?php include('header.php'); ?>
 
+    <section class="commonbanner commonbanner48">
+        <div class="container absolute-center">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-12">
+                    <div class="banner-content">
+                        <h1>Corporate & Health</h1>
+                        <p>Increase effectiveness, support decision making, accelerate R&D and innovation</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
+    <section class="white-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9 col-md-9 col-12"> 
+                    <p class="textinfo">Springer Nature is the world’s largest and most influential publisher of scientific & technical books, journals, databases, and open research. Subscriptions to our online library & databases support technical learning and research at companies of all sizes, from Hardware/Software to the Life Sciences.</p>
+                    <p>This trusted source helps to increase effectiveness, support decision making, accelerate R&D and innovation. Browse our content highlights below and get in touch with us to learn more.</p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-12"> 
+                    <div class="blue-wrapper border-radius">
+                        <a href="#">
+                            <div class="blue-column">
+                                <div class="blue-body">
+                                    <h4>Get in touch!</h4>
+                                </div>
+                            </div>
+                        </a>
+                            <div class="right-icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="gray-section">
+        <div class="container">
+            <div class="row mb-4">
+                <h4 class="subheading border-white">Topic highlights</h4>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12 mb-20">
+                    <div class="blog-wrapper">
+                        <a href="transfer-desk-for-editors.php">
+                            <div class="blog-column">
+                                <div class="blog-header">
+                                    <img src="assets/images/blog/193.webp" class="img-fluid" alt="">
+                                </div>
+                                <div class="blog-body">
+                                    <h3 class="blog-heading">DEI in Research Publishing</h3>
+                                    <p>Editors play a pivotal role in championing inclusive practices and ensuring we reflect our diverse communities through our publication process.</p>
+                                </div>
+                            </div>
+                        </a>
+                            <div class="right-icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                    </div>
+               </div>     
+               <div class="col-lg-6 col-md-6 col-12 mb-20">
+                    <div class="blog-wrapper">
+                        <a href="transfer-desk-for-editors.php">
+                            <div class="blog-column">
+                                <div class="blog-header">
+                                    <img src="assets/images/blog/194.webp" class="img-fluid" alt="">
+                                </div>
+                                <div class="blog-body">
+                                    <h3 class="blog-heading">DEI in Research Publishing</h3>
+                                    <p>Editors play a pivotal role in championing inclusive practices and ensuring we reflect our diverse communities through our publication process.</p>
+                                </div>
+                            </div>
+                        </a>
+                            <div class="right-icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                    </div>
+               </div>                   
+            </div>
+        </div>
+    </section>
+
+    <section class="white-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-8 col-12">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-12">
+                            <div class="initiative-wrapper border-radius">
+                                <a href="#">
+                                    <div class="initiative-column">
+                                        <div class="initiative-header">
+                                            <img src="assets/images/blog/195.webp" class="img-fluid" alt="">
+                                        </div>
+                                        <div class="initiative-body">
+                                            <h3 class="initiative-heading">Women in Science </h3>
+                                            <p>Women have come a long way in enriching the scientific community - yet there's still more to be achieved. We are recognizing women researchers.</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-12">
+                            <div class="initiative-wrapper border-radius">
+                                <a href="#">
+                                    <div class="initiative-column">
+                                        <div class="initiative-header">
+                                            <img src="assets/images/blog/196.webp" class="img-fluid" alt="">
+                                        </div>
+                                        <div class="initiative-body">
+                                            <h3 class="initiative-heading">Women in Science </h3>
+                                            <p>Women have come a long way in enriching the scientific community - yet there's still more to be achieved. We are recognizing women researchers.</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="blue-wrapper border-radius">
+                            <a href="#">
+                                <div class="blue-column">
+                                    <div class="blue-header">
+                                        <img src="assets/images/icons/icon4.webp" class="img-fluid" alt="">
+                                    </div>
+                                    <div class="blue-body">
+                                        <h4>Build a strong editorial board</h4>
+                                        <p>Your journal’s board should be made-up of academic peers who you trust to uphold the credibility of your journal. </p>
+                                    </div>
+                                </div>
+                            </a>
+                                <div class="right-icon">
+                                    <i class="fa-solid fa-angle-right"></i>
+                                </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="gray-section social-background">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="subheading border-white">Stay up to date</h3>
+                </div>
+            </div>
+            <div class="row justify-content-between gx-2">
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-twitter"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Follow us on Twitter</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Follow us on Facebook</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-linkedin-in"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Connect on LinkedIn</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-youtube"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Watch us on YouTube</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-instagram"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Watch us on YouTube</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php include('footer.php'); ?>
---
+++ product-platforms.php
@@ -0,0 +1,129 @@
+<?php include('header.php'); ?>
+
+<style>
+    .column a
+    {
+        text-decoration:none;
+    }
+</style>
+
+<section class="commonbanner commonbanner49">
+    <div class="container absolute-center">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-12">
+                <div class="banner-content">
+                    <h1>Explore our content</h1>
+                    <p>Browse valuable, relevant and up-to-date content on our leading platforms.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="white-section">
+    <div class="container">
+        <div class="row">
+            <div class="ol-lg-9 col-md-9 col-12">
+                <p class="textinfo">nature.com provides access to all Nature Portfolio publications and services, including news and comment from Nature, the # 1 weekly interdisciplinary science journal. You will also find a broad range of specialized titles known as the Academic journals on nature.com.</p>
+                <p class="textinfo">BioMedCentral.com is dedicated to open research and publishes over 290 quality peer-reviewed journals in Biology, Clinical Medicine and Health.</p>
+                <p class="textinfo">ScientificAmerican.com the leading source and authority for science, technology information and policy for a general audience.</p>
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-12">
+                <div class="column background-yellow">
+                    <a href="librarians-products.php">
+                        <h3>For librarians</h3>
+                        <p>Find more information about our products and licensing options on our librarian pages.</p>
+                        <div class="icon">
+                            <i class="fa-solid fa-angle-right"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="social-background">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="subheading border-white">Stay up to date</h3>
+                </div>
+            </div>
+            <div class="row justify-content-between gx-2">
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-twitter"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Follow us on Twitter</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Follow us on Facebook</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-linkedin-in"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Connect on LinkedIn</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-youtube"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Watch us on YouTube</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-12 m20">
+                    <div class="sc-links">
+                        <a href="#">
+                            <div class="socialbg-icon">
+                                <div class="social-icon1">
+                                    <i class="fa-brands fa-instagram"></i>
+                                </div>
+                                <div class="social-icon1txt">
+                                    <p>Watch us on YouTube</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php include('footer.php'); ?>
