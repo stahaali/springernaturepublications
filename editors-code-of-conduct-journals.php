40d64d9a85c0e82e86b2438f4c78e50ef99b730c
asteriksdevteam <devteam@asteriksdigital.com>
2024-12-29 22:46:20 -0800

    commint ok

---
+++ editors-code-of-conduct-journals.php
@@ -0,0 +1,310 @@
+
+<?php include('header.php'); ?>
+
+<style>
+    .column .textgray
+    {
+        color:#444;
+    }
+    .column h3
+    {
+        font-size:18px;
+    }
+    .column .textgray2
+    {
+        color:#444;
+    }
+    .blue-wrapper
+    {
+        height:auto;
+    }
+    .blue-wrapper h3
+    {
+        color:#fff;
+        font-size:20px;
+        margin-top:20px;
+    }
+</style>
+
+<section class="commonbanner commonbanner42">
+    <div class="container absolute-center">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-12">
+                <div class="banner-content">
+                    <h1>Course on code of conduct for journals</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="white-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-12">
+                <p class="textinfo">The Springer Nature Journal Editors' Code of Conduct sets out the standards and expectations associated with the role of Editor-in-Chief. This free course provides you with an overview of the Code.</p>
+
+                <div class="row mt-4">
+                    <div class="col-lg-4 col-md-6 col-12 mb-20">
+                        <img src="assets/images/blog/184.webp" class="img-fluid" alt="">
+                        <p>Editor Resources</p>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12 mb-20">
+                        <img src="assets/images/blog/185.webp" class="img-fluid" alt="">
+                        <p>English</p>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12 mb-20">
+                        <img src="assets/images/blog/186.webp" class="img-fluid" alt="">
+                        <p>45 minutes</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="blue-wrapper border-radius">
+                    <a href="#">
+                        <div class="blue-column">
+                            <img src="assets/images/blog/187.webp" class="img-fluid" alt="">
+                            <h3>Access the course</h3>
+                            <div class="right-icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div> 
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="gray-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-d-4 col-12">
+                <div class="column border-radius bg-white">
+                    <h3 class="textgray">About this course</h3>
+                    <p class="textgray2">Are you unsure about the Journal Editors' Code of Conduct? Dislike reading lengthy documents, but keen to learn about the key concepts in an informative and engaging manner? This course is designed and written for Editors-in-Chief, to help you understand the basic principles of the Journal Editors' Code of Conduct, and also to test your understanding of some tricky scenarios you may face in the course of your editorship.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-d-4 col-12">
+                <div class="column border-radius bg-white">
+                    <h3 class="textgray">Who should take this course?</h3>
+                    <p class="textgray2">This course has been written with Editors-in-Chief in mind, and all Editors-in-Chief should take this course to ensure they are familiar and comfortable with the Journal Editors' Code of Conduct. However, it may also be useful for any Editor who handles manuscripts for a journal and thus all Editors are welcome to take this course. You can take this course in one go or at your own pace, as it is fine to leave and return to it at another time.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-d-4 col-12">
+                <div class="column border-radius bg-white">
+                    <h3 class="textgray">What will I learn in this course?</h3>
+                    <p class="textgray2">You will be taken through the fundamental concepts of the Journal Editors' Code of Conduct to ensure you understand the basic principles of editorial policies, manuscript handling (including peer review and decision making), conflicts of interest, libellous and defamatory material, field-specific policies and post publication issues. You will be presented with interactive questions and exercises to challenge yourself on how to navigate difficult situations.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section cassl="white-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-12">
+                <img src="assets/images/blog/183.webp" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg-9 col-md-9 col-12">
+                <h3 class="subheading mb-4">SpringerNature Code of Conduct page</h3>
+                <p class="textinfo mb-4">In one respect the role of an Editor-in-Chief is fairly straightforward: to ensure that the journal publishes good quality research. However the position is multi-faceted and requires Editors-in-Chief to perform a variety of tasks and interact with a wide range of people. </p>
+                <div class="buttonlinks">
+                    <div class="button079"> 
+                        <a href="#">SpringerNature Code of Conduct page</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="gray-section">
+    <div class="container">
+        <div class="row mb-4">
+            <h3 class="subheading border-white">Other e-learning courses</h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-12 mb-20">
+                <div class="editors-wrapper">
+                    <a href="information-guide.php">
+                        <div class="editors-column bg-white">
+                        <div class="editors-header"> 
+                            <img src="assets/images/editors/1.webp" class="img-fluid" alt="">
+                        </div>
+                        <div class="editors-body">
+                            <h4>Editing a journal</h4>
+                            <p>This guide for Journal Editors provides an overview of the role of Editor-in-Chief, advice on setting up and maintaining an Editorial Board, how to launch and manage a journal as well as develop an existing journal to achieve its full potential.</p>
+
+                            <div class="right-icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-20">
+                <div class="editors-wrapper">
+                    <a href="information-guide.php">
+                        <div class="editors-column bg-white">
+                        <div class="editors-header"> 
+                            <img src="assets/images/editors/1.webp" class="img-fluid" alt="">
+                        </div>
+                        <div class="editors-body">
+                            <h4>Editing a journal</h4>
+                            <p>This guide for Journal Editors provides an overview of the role of Editor-in-Chief, advice on setting up and maintaining an Editorial Board, how to launch and manage a journal as well as develop an existing journal to achieve its full potential.</p>
+
+                            <div class="right-icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-20">
+                <div class="editors-wrapper">
+                    <a href="information-guide.php">
+                        <div class="editors-column bg-white">
+                        <div class="editors-header"> 
+                            <img src="assets/images/editors/1.webp" class="img-fluid" alt="">
+                        </div>
+                        <div class="editors-body">
+                            <h4>Editing a journal</h4>
+                            <p>This guide for Journal Editors provides an overview of the role of Editor-in-Chief, advice on setting up and maintaining an Editorial Board, how to launch and manage a journal as well as develop an existing journal to achieve its full potential.</p>
+
+                            <div class="right-icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-20">
+                <div class="editors-wrapper">
+                    <a href="information-guide.php">
+                        <div class="editors-column bg-white">
+                        <div class="editors-header"> 
+                            <img src="assets/images/editors/1.webp" class="img-fluid" alt="">
+                        </div>
+                        <div class="editors-body">
+                            <h4>Editing a journal</h4>
+                            <p>This guide for Journal Editors provides an overview of the role of Editor-in-Chief, advice on setting up and maintaining an Editorial Board, how to launch and manage a journal as well as develop an existing journal to achieve its full potential.</p>
+
+                            <div class="right-icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-20">
+                <div class="editors-wrapper">
+                    <a href="information-guide.php">
+                        <div class="editors-column bg-white">
+                        <div class="editors-header"> 
+                            <img src="assets/images/editors/1.webp" class="img-fluid" alt="">
+                        </div>
+                        <div class="editors-body">
+                            <h4>Editing a journal</h4>
+                            <p>This guide for Journal Editors provides an overview of the role of Editor-in-Chief, advice on setting up and maintaining an Editorial Board, how to launch and manage a journal as well as develop an existing journal to achieve its full potential.</p>
+
+                            <div class="right-icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-white social-background">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="subheading">Stay up to date</h3>
+            </div>
+        </div>
+        <div class="row justify-content-between gx-2">
+            <div class="col-lg-2 col-md-3 col-12 m20">
+                <div class="sc-links">
+                    <a href="#">
+                        <div class="socialbg-icon">
+                            <div class="social-icon1">
+                                <i class="fa-brands fa-twitter"></i>
+                            </div>
+                            <div class="social-icon1txt">
+                                <p>Follow us on Twitter</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-12 m20">
+                <div class="sc-links">
+                    <a href="#">
+                        <div class="socialbg-icon">
+                            <div class="social-icon1">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </div>
+                            <div class="social-icon1txt">
+                                <p>Follow us on Facebook</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-12 m20">
+                <div class="sc-links">
+                    <a href="#">
+                        <div class="socialbg-icon">
+                            <div class="social-icon1">
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </div>
+                            <div class="social-icon1txt">
+                                <p>Connect on LinkedIn</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-12 m20">
+                <div class="sc-links">
+                    <a href="#">
+                        <div class="socialbg-icon">
+                            <div class="social-icon1">
+                                <i class="fa-brands fa-youtube"></i>
+                            </div>
+                            <div class="social-icon1txt">
+                                <p>Watch us on YouTube</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-12 m20">
+                <div class="sc-links">
+                    <a href="#">
+                        <div class="socialbg-icon">
+                            <div class="social-icon1">
+                                <i class="fa-brands fa-instagram"></i>
+                            </div>
+                            <div class="social-icon1txt">
+                                <p>Watch us on YouTube</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
