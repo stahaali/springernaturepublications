63842d153fc4ce6798cafe403132e271f1a42a80
asteriksdevteam <devteam@asteriksdigital.com>
2024-12-30 23:05:40 -0800

    commint ok

--- product-platforms.php
+++ product-platforms.php
@@ -5,6 +5,16 @@
     {
         text-decoration:none;
     }
+    .coreist:before 
+    {
+        top: 0px;
+        height: 100%;
+    }
+  
+    .coreist2:before
+    {
+        background:#fff;
+    }
 </style>
 
 <section class="commonbanner commonbanner49">
@@ -38,6 +48,240 @@
                             <i class="fa-solid fa-angle-right"></i>
                         </div>
                     </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="gray-section">
+    <div class="container">
+        <div class="row mb-4">
+            <h3 class="subheading border-white mb-4">Springer Nature Link</h3>
+        </div>
+        <div class="row mt-4">
+            <div class="col-lg-9 col-md-9 col-12">
+                <div class="row">
+                    <div class="col-lg-11">
+                        <p>link.springer.com delivers fast, accurate access to the depth and breadth of our online collection of Science, Technology and Medicine, and Humanities and Social Sciences.</p>
+
+                        <div class="list-1">
+                            <ul>
+                            <li><i class="fa-solid fa-check"></i> Publishing the world’s leading scholars across Springer, Palgrave Macmillan, Adis, BMC and Apress publications.</li>
+                            <li><i class="fa-solid fa-check"></i> Includes all content from journals, books, reference works & protocols.</li>
+                            <li><i class="fa-solid fa-check"></i> Content added daily, with more than 12,000 new eBooks, 3,000 new protocols and over 300,000 journal articles every year.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-lg-6">
+                        <div class="openaccess-wrapper">
+                            <a href="https://link.springer.com/">
+                                <div class="openaccess-column">
+                                    <h3>Browse link.springer.com</h3>
+                                    <div class="right-icon">
+                                        <i class="fa-solid fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12">
+                <p><a href="https://link.springer.com/" class="textblue text-deco"><img src="assets/images/brands/18.webp" class="img-fluid" alt=""/></a></p>
+                <div class="coreist coreist2 mt-4 px-3">
+                    <h4>7.5 million +</h4>
+                    <p>Articles</p>
+
+                    <h4>4.8 million +</h4>
+                    <p>Chapters</p>
+
+                    <h4>665000 +</h4>
+                    <p>Reference work entries</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="white-section">
+    <div class="container">
+        <div class="row mb-4">
+            <h3 class="subheading">Nature.com</h3>
+        </div>
+        <div class="row mt-4">
+            <div class="col-lg-9 col-md-9 col-12">
+                <div class="row">
+                    <div class="co-lg-11">
+                        <p class="textinfo">nature.com provides access to Nature Portfolio publications and services, including news and comment from Nature, and the leading scientific jobs board Nature Careers. It also provides access to academic and society journals published in collaboration with Springer Nature. </p>
+
+                        <div class="list-1 list-3">
+                            <ul>
+                                <li><i class="fa-solid fa-check"></i>  Access all Nature Research content including nature, Nature journals, Nature Review journals, adding more than 35,000 research articles every year.</li>
+                                <li><i class="fa-solid fa-check"></i>  Includes the open access content of Nature Communications, the Nature Partner Journals and Scientific Reports.   </li>
+                                <li><i class="fa-solid fa-check"></i>  Read the latest updates from our editors and science news.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div class="openaccess-wrapper">
+                            <a href="/gp/librarians/products/journals/nature-research-journals">
+                                <div class="openaccess-column">
+                                    <h3>Nature Research journals</h3>
+                                    <div class="right-icon">
+                                        <i class="fa-solid fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div class="openaccess-wrapper">
+                            <a href="/gp/librarians/products/journals/academic-journals-on-nature-com">
+                                <div class="openaccess-column">
+                                    <h3>Academic journals</h3>
+                                    <div class="right-icon">
+                                        <i class="fa-solid fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12">
+                <div class="coreist mt-4 px-3">
+                    <h4>1 million +</h4>
+                    <p>articles</p>
+
+                    <h4>11 million +</h4>
+                    <p>average visitors per week</p>
+
+                    <h4>133 million +</h4>
+                    <p>average visitors per month</p>
+
+                    <div class="button079 mt-4">
+                        <a href="/gp/librarians/products/platforms/updates-to-nature-com">Improving nature.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="gray-section">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-9 col-md-9 col-12">
+
+                <p class="textinfo">BioMedCentral.com provides access to a large portfolio of peer-reviewed open access journals. Dedicated to open research, BioMed Central now publish over 290 quality peer-reviewed journals in Biology, Clinical Medicine and Health.</p>
+                <p>All our platforms combine access to leading peer reviewed publications with intuitive functionality so users can find and access content fast and effectively:</p>
+
+                <div class="list-1">
+                    <ul>
+                        <li><i class="fa-solid fa-check"></i> A mobile optimized experience allowing you to read content on any device.</li>
+                        <li><i class="fa-solid fa-check"></i> Access to peer-reviewed articles before they are published in print.</li>
+                        <li><i class="fa-solid fa-check"></i> Alert services to keep you up-to-date on the latest research in your area of interest. </li>
+                        <li><i class="fa-solid fa-check"></i> Print-on-demand service for selected content.</li>
+                        <li><i class="fa-solid fa-check"></i> Access to supplementary materials including videos</li>
+                    </ul>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div class="openaccess-wrapper">
+                            <a href="https://www.biomedcentral.com/journals">
+                                <div class="openaccess-column">
+                                    <h3>Journals by discipline</h3>
+                                    <div class="right-icon">
+                                        <i class="fa-solid fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div class="openaccess-wrapper">
+                            <a href="https://www.biomedcentral.com/journals-a-z">
+                                <div class="openaccess-column">
+                                    <h3>Journals A-Z</h3>
+                                    <div class="right-icon">
+                                        <i class="fa-solid fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-3 col-md-3 col-12">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <a href="https://www.biomedcentral.com"><img src="assets/images/brands/3.webp" class="img-fluid" alt=""/></a>
+                    </div>
+                </div>
+                <h4 class="textgray mb-4 mt-4">We have</h4>
+                <div class="coreist coreist2 mt-4 px-3">
+                    <h4>70000 +</h4>
+                    <p>articles</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="white-section">
+    <div class="container">
+        <div class="row mb-4">
+            <h3 class="subheading">Scientific American</h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-9 col-md-9 col-12">
+
+                <p>Scientific American is the leading source and authority for science, technology information and policy for a general audience. </p>
+
+                <div class="list-1 list-3">
+                    <ul>
+                        <li><i class="fa-solid fa-check"></i> First published in 1845, Scientific American is the longest continuously published magazine in the US. </li>
+                        <li><i class="fa-solid fa-check"></i> Since then the title has published articles from over 150 Nobel Prize scientists and built a loyal following of influential and forward thinking readers</li>
+                    </ul>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="col-lg-7">
+                        <div class="openaccess-wrapper">
+                            <a href="http://www.scientificamerican.com">
+                                <div class="openaccess-column">
+                                    <h3>Browse ScientificAmerican.com</h3>
+                                    <div class="right-icon">
+                                        <i class="fa-solid fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12">
+                <a href="https://media.springernature.com/full/springer-cms/rest/v1/content/12266704/data/v2"><img src="assets/images/brands/17.webp" class="img-fluid" alt=""/></a>
+                <h4 class="textgray mb-4 mt-4">We have</h4>
+                <div class="coreist mt-4 px-3">
+                    <h4>8.3 million +</h4>
+                    <p>website visitors per month</p>
+
+                    <h4>7.6 million +</h4>
+                    <p>social media followers</p>
+
+                    <h4>14 +</h4>
+                    <p>translated editions</p>
+
                 </div>
             </div>
         </div>
@@ -124,6 +368,6 @@
                 </div>
             </div>
         </div>
-    </section>
+</section>
 
 <?php include('footer.php'); ?>
